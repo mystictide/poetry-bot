3e5e19d7ff06e15024c6f558a0b1aa024602ca2d
Furkan Güler <furkang2k20@outlook.com>
2022-12-20 11:28:58 +0300

    cyclic

--- server/server.js
+++ server/server.js
@@ -10,14 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-twit.poemPoster();
-setInterval(function () {
-    twit.poemPoster();
-}, 11000000);
+// twit.poemPoster();
+// setInterval(function () {
+//   twit.poemPoster();
+// }, 11000000);
 
 app.use(errorHandler);
-app.get('/', (req, res) => {
-    res.send('orpheus laments')
-  })
+app.get("/", (req, res) => {
+  twit.poemPoster();
+  res.send("orpheus laments");
+});
 app.use("/static", express.static(__dirname + "/fonts"));
 app.listen(port, () => console.log(`server started on port: ${port}`));
